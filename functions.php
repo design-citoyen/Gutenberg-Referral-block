543ec5d862644ede27b7061f5dd407a6c27ac5a9
Fernando Garcia <salarrue@users.noreply.github.com>
2020-08-12 22:40:34 -0400

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,20 @@
+
+//Add new block type
+add_action('acf/init', 'my_acf_init_block_types');
+function my_acf_init_block_types() {
+
+    // Check function exists.
+    if( function_exists('acf_register_block_type') ) {
+
+        // register a referral block.
+        acf_register_block_type(array(
+            'name'              => 'referral',
+            'title'             => __('Referral'),
+            'description'       => __('A custom testimonial block.'),
+            'render_template'   => 'template-parts/blocks/testimonial/referral.php',
+            'category'          => 'formatting',
+            'icon'              => 'admin-comments',
+            'keywords'          => array( 'referral', 'link' ),
+        ));
+    }
+}
